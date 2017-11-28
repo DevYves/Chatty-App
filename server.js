b67725f47e0beeba5d5ff1d6434f850012fba3e5
DevYves <yfreypons@gmail.com>
2017-11-28 21:18:09 +0000

    Setup state and passed props to messages and chatbar - added starter database
    Allows React to generate the DOM as it gets new info from the Db rather than hard code it

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var react = require('react');
+var reactDOMserver = require('react-dom/server');
 
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx'
+import ChatBar from './ChatBar.jsx';
+
+const dataBase = {
+  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    {
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+      id: 1
+    },
+    {
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+      id: 2
+    }
+  ]
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state =
+      dataBase;
+
+}
+
   render() {
+    console.log("Rendering <App/>");
+    console.log(this.state.messages);
     return (
-      <h1>Hello React :)</h1>
+    <div>
+      <MessageList messages = {this.state.messages}/>
+      <ChatBar currentUser={this.state.currentUser}/>
+    </div>
     );
   }
 }
 export default App;
+
+
